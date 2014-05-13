fdc196164c97a55a181e24562333a9eed2ef62fa
Lee Yang <gnayrr@gmail.com>
2014-05-13 16:38:03 -0700

    update folder structure and minify js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,32 @@ module.exports = function (grunt) { 'use strict';
 
 		pkg: grunt.file.readJSON('package.json'),
 
+		uglify: {
+
+			options: {
+				compress: {
+					drop_console: true
+				}
+			},
+
+			listr: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src: '**/*.js',
+						dest: 'dist/',
+						ext: '.min.js'
+					}
+				]
+			}
+		},
+
 		jshint: {
 			
 			all: [
 				'Gruntfile.js',
-				'lib/**/listr.js',
+				'src/**/listr.js',
 				'test/**/*.js'
 			],
 
@@ -18,7 +39,7 @@ module.exports = function (grunt) { 'use strict';
 		},
 
 		jasmine: {
-			src: 'lib/**/*.js',
+			src: 'src/**/*.js',
 
 			options: {
 				specs: 'test/**/*.js',
@@ -27,9 +48,12 @@ module.exports = function (grunt) { 'use strict';
 		}
 	});
 
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 
 	grunt.registerTask('test', ['jshint', 'jasmine']);
+	grunt.registerTask('release', ['test', 'uglify']);
 	grunt.registerTask('default', ['test']);
 };
